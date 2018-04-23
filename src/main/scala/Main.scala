90722fc28c5375031dc9b409b4b7736fcd41773c
peter <deamondev@gmail.com>
2018-04-23 08:14:08 +0200

    OK

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,4 +6,8 @@ object Main extends App {
 
   val myList: List[Int] = List(1,3,10, -17)
   println(P1.last(myList))
+  println(P2.almostLast(myList))
+  println(P3.reverse(myList))
+  println(P3.lastNth(myList, 2))
+  println(P3.lastNth(P3.reverse(myList), 2))
 }
